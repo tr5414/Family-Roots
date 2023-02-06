4630176d8a2dbe0b8aa3745c23f3f44ac9e8cd3b
d <d>
2023-02-05 16:43:15 -0800

    Hacked together the end results into main scene.

--- Assets/FamilyTree/Scripts/ExitDoor/ExitDoorUI.cs
+++ Assets/FamilyTree/Scripts/ExitDoor/ExitDoorUI.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ExitDoorUI : MonoBehaviour
@@ -13,10 +14,13 @@ public class ExitDoorUI : MonoBehaviour
     public GameObject fadeOutContainer;
 
     public GameObject resultsContainer;
+    public ResultsScreenPrompt resultsScreen;
 
     private StarterAssets.StarterAssetsInputs starterAssetsInputs;
 
     private PlayerControlLocking controlLock;
+
+    private bool isMidPrompt = false;
 
     void Awake()
     {
@@ -30,22 +34,40 @@ public class ExitDoorUI : MonoBehaviour
             starterAssetsInputs = playerObj.GetComponent<StarterAssets.StarterAssetsInputs>();
         }
 
-        exitQuestionCancelButton.onClick.AddListener(Hide);
         controlLock = FindObjectOfType<PlayerControlLocking>();
+
+        exitQuestionCancelButton.onClick.AddListener(Hide);
+        exitQuestionConfirmButton.onClick.AddListener(PromptWithResults);
     }
     public void PromptWithQuestion()
     {
+        if (isMidPrompt)
+            return;
+
         exitQuestionContainer.SetActive(true);
         fadeOutContainer.SetActive(false);
         resultsContainer.SetActive(false);
 
         controlLock.Lock(this);
         canvasObject.gameObject.SetActive(true);
+
+        isMidPrompt = true;
     }
 
     public void PromptWithResults()
     {
-        // TODO
+        //if (isMidPrompt)
+        //    return;
+
+        resultsScreen.EvaluateResults();
+        exitQuestionContainer.SetActive(false);
+        fadeOutContainer.SetActive(true);
+        resultsContainer.SetActive(true);
+
+        //controlLock.Lock(this);
+        canvasObject.gameObject.SetActive(true);
+
+        isMidPrompt = true;
     }
 
     public void Hide()
@@ -56,10 +78,7 @@ public class ExitDoorUI : MonoBehaviour
 
         controlLock.Unlock(this);
         canvasObject.gameObject.SetActive(false);
-    }
 
-    private void EvaluateResults()
-    {
-        // TODO
+        isMidPrompt = false;
     }
 }
--- Assets/FamilyTree/Scripts/ExitDoor/ResultsScreenPrompt.cs
+++ Assets/FamilyTree/Scripts/ExitDoor/ResultsScreenPrompt.cs
@@ -1,16 +1,93 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ResultsScreenPrompt : MonoBehaviour
 {
-    public TMPro.TextMeshPro resultTitle;
+    public TMPro.TextMeshProUGUI resultTitle;
 
-    public TMPro.TextMeshPro textDetails;
+    public TMPro.TextMeshProUGUI textDetails;
 
-    public TMPro.TextMeshPro nameCountText;
+    public TMPro.TextMeshProUGUI nameCountText;
 
-    public TMPro.TextMeshPro childCoundText;
+    public TMPro.TextMeshProUGUI childCountText;
 
     public Button confirmButton;
+
+    public string failureTitle;
+
+    [TextArea]
+    public string failureDescription;
+
+    public string successTitle;
+
+    [TextArea]
+    public string successDescription;
+
+    public string maybeTitle;
+
+    [TextArea]
+    public string maybeDescription;
+
+    private int totalNameConnections;
+    private int totalChildConnections;
+
+    private int nameErrors;
+    private int childErrors;
+
+
+    public void EvaluateResults()
+    {
+        CorkboardFamilyTreeNetwork familyTreeNetwork = FindObjectOfType<CorkboardFamilyTreeNetwork>();
+
+        familyTreeNetwork.GradeConnections(out totalChildConnections, out totalNameConnections, out childErrors, out nameErrors);
+
+        PrintResults();
+
+        if (childErrors > 2)
+        {
+            FailCase();
+        }
+        else if (childErrors <= 1 && nameErrors < 3)
+        {
+            SuccessCase();
+        }
+        else
+        {
+            MediumCase();
+        }
+
+        confirmButton.onClick.AddListener(Exit);
+    }
+    private void PrintResults()
+    {
+        nameCountText.text = $"Names: {totalNameConnections - nameErrors} / {totalNameConnections}";
+        childCountText.text = $"Relations: {totalChildConnections - childErrors} / {totalChildConnections}";
+    }
+
+    private void FailCase()
+    {
+        resultTitle.text = failureTitle;
+        textDetails.text = failureDescription;
+    }
+
+    private void MediumCase()
+    {
+        resultTitle.text = maybeTitle;
+        textDetails.text = maybeDescription;
+    }
+
+    private void SuccessCase()
+    {
+        resultTitle.text = successTitle;
+        textDetails.text = successDescription;
+    }
+
+
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
 
 }
